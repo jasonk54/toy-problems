191a6b43cde38780cca6f1f41d16b44cd1344146
Marcus Phillips <github.com@marcusphillips.com>
2013-02-28 09:25:13 -0800

    linked list cycles problem

---
+++ linkedListCycles/linkedListCycles.js
@@ -0,0 +1,23 @@
+/*
+ * Assignmnet: Write a function that can detect whether or not a linked list has a cycle
+ *
+ * Explanation: A 'cycle' in a linked list is when traversing the list would result in visiting the same nodes over and over
+ * This is caused by pointing a node in the list to another node that already appeared earlier in the list. Example:
+ * node1.next points to node2
+ * node2.next points to node3
+ * node3.next points to node2
+ *
+ * You should write an algorithm that can return true or false, depending on whether the input list has a cycle
+ *
+ * Constraint 1:
+ * Do this in linear time
+ *
+ * Constraint 2:
+ * Do this in constant space
+ *
+ * Constraint 3:
+ * Do not mutate the original nodes in any way
+ */
+var hasCycle = function(linkedList){
+  // ...?
+};
