c2a58d8406d7dd1e1895db433914e94bb3f4c474
Marcus Phillips <github.com@marcusphillips.com>
2013-03-11 09:08:56 -0700

    get elements by class name problem

---
+++ getElementByClassName/problem.js
@@ -0,0 +1,7 @@
+// The browser comes with a utility function getElementById that returns an element whose id matches the input.
+// Write a function that returns an array of all nodes that match a particular class name
+// To get started, you may use jQuery, but try to refactor it out once you have it working
+
+var getElementsByClassName = function(className){
+  // todo!
+};
