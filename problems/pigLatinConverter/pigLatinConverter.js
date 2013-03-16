f50e520360fc4962053b21fa9590b12a1af11066
Nathan Houle <nathan@nathanhoule.com>
2013-03-15 23:27:36 -0700

    Basic skeleton for a pig latin converter function

---
+++ problems/pigLatinConverter/pigLatinConverter.js
@@ -0,0 +1,23 @@
+// Write a function that takes an English word as a string and converts it to
+// Pig Latin. Your Pig Latin implementation should follow two basic rules: For
+// words that begin with consonants, move the first letter to the end of the
+// word and add "ay" onto the end. For example:
+//
+//   "happy" → "appyhay"
+//
+// For words that begin with vowel sounds, add "ay" to the end of the word.
+// For example:
+//
+//   "duck" → "uckday"
+//
+//
+// Extra credit: Make your function accept a string that contains multiple
+// words.
+//
+// Extra extra credit: Write a function that detects Pig Latin and converts
+// it back to English.
+
+
+var convertToPigLatin = function(string) {
+  // Your code here
+};
