c8f898a2882ab71a381f409cecad6128a77183d5
Jason K <jasonk54@gmail.com>
2013-03-18 10:04:03 -0700

    Moved changes over from other repo
    implementing operators WIP

--- problems/implementingOperators/implementingOperators.js
+++ problems/implementingOperators/implementingOperators.js
@@ -9,13 +9,41 @@
 // Terror mode: Re-implement all three functions using only bitwise operators.
 
 var multiply = function(x, y) {
-  // Your code goes here
+	if (x === 1 && y === 1) {
+		return x;
+	}
+	var result = 0;
+	for (var i = 0; i < x; i++) {
+		result += y;
+	}
+	return result;
 };
 
 var divide = function(x, y) {
-  // Your code goes here
+	if (x === 1 && y === 1) {
+		return x;
+	}
+	if (x === 0 || y === 0) {
+		return 0;
+	}
+	var result = 1;
+	for (var i = y; i < x; i+= y) {
+		result++;
+	}
+	return result;
 };
 
 var modulo = function(x, y) {
-  // Your code goes here
+	var temp = multiply(divide(x,y), y);
+	var z = temp - y;
+	if (temp > x) {
+		var result = x - z;
+	} else {
+		return 0;
+	}
+	return result;
 };
+
+
+y = 4
+12 - 3 = 9
