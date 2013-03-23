e5aa5eccca6d8e547d6975af6af3c17aa370a7a9
Nathan Houle <nathan@nathanhoule.com>
2013-03-23 09:03:17 -0700

    Added integerGenerator problem

---
+++ problems/integerGenerator/integerGenerator.js
@@ -0,0 +1,12 @@
+/**
+ * Given a function which produces a random integer in the range 1 to 5, write
+ * a function which produces a random integer in the range 1 to 7.
+ */
+
+var random1to5 = function() {
+ // Your code goes here
+};
+
+var random1to7 = function() {
+  // Your code goes here. Use the function above to implement this function.
+};
