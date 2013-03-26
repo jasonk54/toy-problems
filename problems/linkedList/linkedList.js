e6f0d9df7d770ab22d0d530bc90682232ca3d9b3
Nathan Houle <nathan@nathanhoule.com>
2013-03-26 08:05:28 -0700

    Added linked list problem

---
+++ problems/linkedList/linkedList.js
@@ -0,0 +1,12 @@
+/**
+ * Working with a partner, implement a function that creates a linked list.
+ * While you're navigating, don't touch the keyboard once--verbally walk your
+ * partner through implementing the linked list and tell them why you're making
+ * the choices you're making.
+ *
+ * Extra credit: Modify your linked list to be a doubly linked list.
+ */
+
+var makeLinkedList = function() {
+  // Your code here
+};
