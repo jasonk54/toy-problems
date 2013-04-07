f071d3636d21573de2b44d5ca650d43f6eb56aaf
Nathan Houle <nathan@nathanhoule.com>
2013-04-07 08:55:21 -0700

    Reformatted text

--- problems/spiralTraversal/spiralTraversal.js
+++ problems/spiralTraversal/spiralTraversal.js
@@ -1,17 +1,15 @@
-/*
- * Write a function that accepts a 2-dimensional array (that is, an array containing many same-length arrays),
- * and prints out every value found, but in a spiral from the upper left in to the center
- * Please write some form of specs, tests, or assertions for your code, and check as many edge cases as you can think of
+/** Write a function that accepts a 2-dimensional array (that is, an array
+ * containing many same-length arrays), and prints out every value found, but in
+ * a spiral from the upper left in to the center Please write some form of
+ * specs, tests, or assertions for your code, and check as many edge cases as
+ * you can think of
  *
- * example:
-traverseSpirally([
-  [1,2,3],
-  [4,5,6],
-  [7,8,9]
-]);
+ * For example:
+ *   traverseSpirally([ [1,2,3], [4,5,6], [7,8,9] ]);
  * would result in printing:
-1 2 3 6 9 8 7 4 5
+ *   1 2 3 6 9 8 7 4 5
  */
+
 var traverseSpirally = function(matrix){
-  // ...?
+  // Your code here
 };
