e8388aa6c377ba7a3d49105697a766fcd3bc4222
Nathan Houle <nathan@nathanhoule.com>
2013-04-08 01:40:30 -0700

    Added filler text generator problem

---
+++ problems/fillerTextGenerator/src/fillerTextGenerator.js
@@ -0,0 +1,22 @@
+/**
+ * Today's toy problem is to write a function that can create filler text, i.e.
+ * text that doesn't actually mean anything, but from a distance could plausibly
+ * look like real language.
+ *
+ * Implement these features in order:
+ * - The argument to function is the number of words the function should return.
+ * - The text is made up of sentences with 3-8 words each.
+ * - Each word is made up of 1-12 random characters from A-Za-z.
+ * - Sentences have first word capitalized and a period at the end.
+ * - After each sentence there should be a 15% chance of a newline.
+ * - Instead of periods after each sentence, introduce other punctuation marks
+ *   (e.g. question marks and exclamation marks).
+ * - Put some of the words in "quotes." People love "quotes."
+ *
+ * If you finish all of this, look into replacing your random letters with
+ * pseudo-natural language.
+ */
+
+var generateFillerText = function(wordCount) {
+  // Your code here
+};
