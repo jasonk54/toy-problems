7386f0b9dba2e337476c863fb2fb354251408800
Nathan Houle <nathan@nathanhoule.com>
2013-04-12 09:07:55 -0700

    Added delayed textbox problem

---
+++ problems/delayedTextbox/src/delayedTextbox.js
@@ -0,0 +1,8 @@
+/**
+ * We have a textbox where we get the info with the following code:
+ *
+ *   var tb = $(".textbox");
+ *
+ * We want to get the text of that box, but only 600 ms after the person stops
+ * typing in that box. How would you implement it?
+ */
