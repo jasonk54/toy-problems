fba46dd906d157cb26ba65cef7644c8f085a4f1d
Nathan Houle <nathan@nathanhoule.com>
2013-05-12 01:34:52 -0700

    Add linkedListReversal problem

---
+++ problems/linkedListReversal/src/linkedListReversal.js
@@ -0,0 +1,18 @@
+/**
+ * Implement a linked list. Once you've done that, write a function that
+ * performs an in-place reversal on your linked list.
+ *
+ * Extra credit: Rewrite your linked list and reversal function in CoffeeScript.
+ */
+
+var createLinkedList = function() {
+  var linkedList = {};
+
+  // Your supporting code/methods here
+
+  linkedList.reverse = function() {
+    // More code in here
+  };
+
+  return linkedList;
+};
