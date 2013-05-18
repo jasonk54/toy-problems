8a4f1f4ff3fdae192181884fb4746d58707e21e0
Nathan Houle <nathan@nathanhoule.com>
2013-05-18 03:03:40 -0700

    Add reverse array problem

---
+++ problems/reverseArray/src/reverseArray.js
@@ -0,0 +1,15 @@
+/**
+ * Given an arbitrary input array, write a function that reverses the contents
+ * of the array. Don't use the native Array.prototype.reverse() method.
+ *
+ * Extra Credit: Don't use an extra array.
+ *
+ *
+ * Here's a sample input to get you going:
+ *
+ *   [1, 8, 39, null, 2, 9, 'bob']
+ */
+
+var reverseArray = function(array) {
+  // Your code here
+};
