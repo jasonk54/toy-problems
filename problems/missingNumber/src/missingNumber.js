119f6734c81fadba2ec80753e640d612423d376f
Nathan Houle <nathan@nathanhoule.com>
2013-05-19 09:22:47 -0700

    Clear up/clean up prompt

--- problems/missingNumber/src/missingNumber.js
+++ problems/missingNumber/src/missingNumber.js
@@ -1,6 +1,7 @@
 /**
  * You're given an array every number between 1 and 100,000, except one integer
- * is missing. Write a function to detect which
+ * is missing. The numbers are not guaranteed to be consecutive, either! Write a
+ * function to detect which number is missing.
  *
  * Extra credit: Refactor your solution to take O(n) time.
  */
