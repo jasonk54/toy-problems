5ff4a8cd420a9631cd2dfc171c6759a5f3a5d889
Nathan Houle <nathan@nathanhoule.com>
2013-05-20 01:38:18 -0700

    Add jsonparty question

---
+++ problems/jsonParty/src/jsonParty.js
@@ -0,0 +1,14 @@
+/**
+ * Implement a function that takes a URL and a callback and makes a JSONP
+ * request. Call your function three times in a row, appending the results to
+ * #container. Ensure that the random number returned by each JSONP request is
+ * unique.
+ *
+ * Use this URL as your sample endpoint:
+ *
+ * http://toy-problems.hackreactor.com/jsonparty
+ */
+
+var jsonpRequest = function(url, callback) {
+  // Your code here
+};
