0b5bf5200250e073b9a0959c61a20dde8850aeb5
Jason K <jasonk54@gmail.com>
2013-11-11 17:32:52 -0800

    bubblesort added

--- problems/bubbleSort/src/bubbleSort.js
+++ problems/bubbleSort/src/bubbleSort.js
@@ -1,50 +1,19 @@
-/**
- * Bubble sort is the most basic sorting algorithm in all of Computer
- * Sciencedom. It works by starting at the first element of an array and
- * comparing it to the second element; if the first element is greater than the
- * second element, it swaps the two. It then compares the second to the third,
- * and the third to the fourth, and so on; in this way, the largest values
- * "bubble" to the end of the array. Once it gets to the end of the array, it
- * starts over and repeats the process until the array is sorted numerically.
- *
- * Implement a function that takes an array and sorts it using this technique.
- *
- * QUERY: What's the time complexity of your algorithm? If you don't already
- * know, try to intuit this without consulting the Googles.
- *
- * Extra credit: Optimization time! During any given pass, if no elements are
- * swapped we can assume the list is sorted and can exit the function early.
- * After this optimization, what is the time complexity of your algorithm?
- *
- * Moar credits: Do you need to consider every element every time you iterate
- * through the array? Make it happen, boss. Again: Has the time complexity of
- * your algorithm changed?
- *
- * Ugh, done already? Convert this functional instantiation pattern into a
- * prototypal or pseudoclassical pattern.
- **/
-
 
 // Creates an instance of bubbleSort. Example usage:
 // var bubbleSort = createBubbleSort();
 // bubbleSort([2, 1, 3]); // yields [1, 2, 3]
-var createBubbleSort = function(array) {
+
+var bubbleSort = function(arr) {
 	var counter = 0;
-	if (Object.prototype.toString.call(array) === "[object Array]") {
-		while (counter < array.length) {
-			for (var i = 0; i < array.length; i++) {
-				if (array[i] > array[i+1]) {
-					var holder = array[i];
-					array[i] = array[i+1];
-					array[i+1] = holder;
-				}
+	while(counter < arr.length) {
+		for (var i = 0; i < arr.length; i++) {
+			if (arr[i] > arr[i+1]) {
+				var temp = arr[i];
+				arr[i] = arr[i+1];
+				arr[i+1] = temp
 			}
-			counter++;
 		}
-		return array;
-	} else {
-		throw new TypeError('Need array');
+		counter++;
 	}
-};
-
-
+	return arr;
+}
