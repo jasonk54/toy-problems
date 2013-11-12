4d77825e26cff4a62350600fcf56d50cc124f287
Jason K <jasonk54@gmail.com>
2013-11-12 12:35:34 -0800

    anagram cleanup

--- problems/allAnagrams/allAnagrams.js
+++ problems/allAnagrams/allAnagrams.js
@@ -3,18 +3,19 @@
  * of a string and outputs them as an array. At first, don't worry about
  * repeated strings.  What time complexity is your solution?
  *
- * Extra credit: Deduplicate your return array without using uniq().
  */
 
 var result = [], usedChars = [];
+
 var allAnagrams = function(str) {
 	var	str = str.split(''),
 		ch;
 
 	for (var i = 0; i < str.length; i++) {
+
 		// Plucks one character at the given index
 		ch = str.splice(i, 1);
-		
+		console.log('str splice beg: ' + ch)	
 		// Push char to usedChars
 		usedChars.push(ch);
 
@@ -24,10 +25,9 @@ var allAnagrams = function(str) {
 		}
 
 		allAnagrams(str.join(''));
+
 		str.splice(i, 0, ch);
 		usedChars.pop();
 	}
 	return result;
 }
-
-
